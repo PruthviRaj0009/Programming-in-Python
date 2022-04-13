df57f573f66532b87bf56a76148f13557df64bc6
Pruthvi Raj <78634459+PruthviRaj0009@users.noreply.github.com>
2022-04-13 13:41:12 +0530

    Create 20CS068_Practical_5.py

---
+++ Practical 5/20CS068_Practical_5.py
@@ -0,0 +1,17 @@
+'''
+20CS068
+Pruthvi Raj
+
+You are given a string and your task is to swap cases.
+In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+Sample Input:
+HackerRank.com presents "Pythonist 2".
+
+Sample Output:
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+'''
+# taking string input from user
+v = input()
+# swaping case and printing output to user
+print(v.swapcase())
